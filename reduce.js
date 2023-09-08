572ed79d80c984b20bd6d13ba8595742709d697b
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-09-08 15:22:21 +0000

    initial commit

--- reduce.js
+++ reduce.js
@@ -6,7 +6,21 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
+// Expanded to see how it works
+const nums1 = [0, 1, 2, 3, 4];
+let sum1 = nums1.reduce((acc, curr) => {
+  console.log(
+    "Accumulator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr;
+}, 10); // if you want to specify an initial value to use as an accumulator
+console.log(sum1);
 
 const teamMembers = [
   {
@@ -32,6 +46,40 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
+
+const groupedByProfession = teamMembers.reduce((acc, member) => {
+  const { name, profession } = member;
+
+  // Check if the profession already exists in the accumulator
+  if (acc.has(profession)) {
+    acc.get(profession).push(name);
+  } else {
+    // If it doesn't exist, create a new entry
+    acc.set(profession, [name]);
+  }
+
+  return acc;
+}, new Map());
+
+// Convert the Map back to an object
+const result = {};
+groupedByProfession.forEach((value, key) => {
+  result[key] = value;
+});
+
+console.log(result);
